d45eba1be9262134048a3f2da9da52d930a33151
Layo Folaranmi <thedarkkid.codes@gmail.com>
2021-03-08 22:35:10 +0100

    0.1.6

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ export * from './Files';
 export * from './Generator';
 export * from './Messages';
 export * from './Utility';
-export {default as Echo} from './Echo';
+export { default as Echo } from './Echo';
 export * from './interfaces';
